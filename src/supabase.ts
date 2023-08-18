4a671773deb9ea040b21e1ea6d141f82e2d54f39
mael098 <juanvillegasmorales@outlook.com>
2023-08-17 22:26:50 -0600

    doungrade

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2.31.0";
-import { Database } from "./database.types.ts";
+import { createClient } from "https://deno.land/x/supabase@1.3.1/mod.ts";
+// import { Database } from "./database.types.ts";
 
-export default createClient<Database>(
+export default createClient(
   Deno.env.get("SUPABASE_URL") as string,
   Deno.env.get("SUPABASE_PUBLIC_KEY") as string,
 );
