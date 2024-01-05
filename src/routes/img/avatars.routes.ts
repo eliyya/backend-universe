8bb1d7ea6f9fe5841b4a638584f7c5b5bf3dda69
Elí Maciel <tecno_eli@hotmail.com>
2024-01-04 19:18:22 -0600

    add 404

--- src/routes/img/avatars.routes.ts
+++ src/routes/img/avatars.routes.ts
@@ -5,8 +5,12 @@ const avatarApi = new Hono()
 
 avatarApi.get('/:id', async (ctx) => {
     const id = ctx.req.param('id')
-    const f = await imgsController.getAvatar(id)
-    return new Response(f)
+    try {
+        return new Response(await imgsController.getAvatar(id))
+    } catch (error) {
+        if (error.toString().includes('NotFound')) return ctx.notFound()
+        else return ctx.text('Internal Server Error', 500)
+    }
 })
 
 export default avatarApi
