6766c092399ca28588a49a2866c3133ce54cf5c1
Elí Maciel <tecno_eli@hotmail.com>
2024-01-06 03:57:23 -0600

    import automactic with script

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,22 @@ app.onError((error, ctx) => {
     return ctx.json({ message: 'Internal server error' }, 500)
 })
 
+// IMPORTANT: DON'T TOUCH THIS LINES
+
 import authApi from './routes/auth.routes.ts'
 app.route('/auth', authApi)
-import avatarApi from './routes/img/avatars.routes.ts'
-app.route('/img/avatars', avatarApi)
+
 import usersApi from './routes/api/users.routes.ts'
 app.route('/api/users', usersApi)
-import groupsApi from './routes/api/groups.routes.ts'
-app.route('/api/groups', groupsApi)
+
 import meApi from './routes/api/users/@me.routes.ts'
 app.route('/api/users/@me', meApi)
+
+import groupsApi from './routes/api/groups.routes.ts'
+app.route('/api/groups', groupsApi)
+
+import avatarsApi from './routes/img/avatars.routes.ts'
+app.route('/img/avatars', avatarsApi)
+
 
 Deno.serve(app.fetch)
